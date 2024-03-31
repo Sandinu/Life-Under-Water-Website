79980ccf75776d92af84ac77ef32f24567cda378
GayaraDiss <sathijadissanayake@gmail.com>
2024-03-31 19:55:07 +0530

    Created the Feedback, content, page editor, check anything that has to be changed

---
+++ Js/pageEditor_Sathija.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Home Page', 'Navigation Bar','Footer', 'Template', 'Feedback', 'Content Page', 'Page Editor'],
+    typeSpeed:100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+})
